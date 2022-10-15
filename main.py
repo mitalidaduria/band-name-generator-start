3f75a9c544ecdd8987849092e133fd6d9d8c6382
mitalidaduria <94062583+mitalidaduria@users.noreply.github.com>
2022-10-15 06:53:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Brand Name Generator")
+
+city_name = input(" \n What city did you grew up in? \n ")
+pet_name = input(" \n What is the name of your pet? \n ")
+
+print('\n Your brand name could be: \n ' + city_name +  pet_name)
